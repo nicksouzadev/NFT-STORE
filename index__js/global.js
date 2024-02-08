54228953991441b24bdbb4448825b6786f34b866
n1cksouza <nicolas.souza.privado@gmail.com>
2024-02-08 10:05:40 -0300

    criando a tela principal, e criando cronometro aleatorio

--- index__js/global.js
+++ index__js/global.js
@@ -1,3 +1,45 @@
+const elements = document.querySelectorAll('.bid p');
+
+const maxTime = 40000; 
+
+function updateTimer(element, time) {
+    const hours = Math.floor(time / 3600);
+    const minutes = Math.floor((time % 3600) / 60);
+    const seconds = time % 60;
+
+    const hoursText = hours > 0 ? `${hours}h ` : '';
+
+    const minutesText = minutes > 0 ? `${minutes}m ` : '';
+
+    element.textContent = `${hoursText}${minutesText}${seconds}s`;
+
+    time--;
+
+    if (time < 0) {
+
+        time = maxTime;
+
+        const randomTime = Math.floor(Math.random() * maxTime) + 1;
+
+        setTimeout(() => {
+            updateTimer(element, randomTime);
+        }, 1000);
+    } else {
+
+        setTimeout(() => {
+            updateTimer(element, time);
+        }, 1000);
+    }
+}
+
+elements.forEach((element) => {
+
+    const initialRandomTime = Math.floor(Math.random() * maxTime) + 1;
+
+    updateTimer(element, initialRandomTime);
+});
+
+
 function menuShow() {
     let menuMobile = document.querySelector('.mobile-menu');
     if (menuMobile.classList.contains('open')) {
